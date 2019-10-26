59468f1fbda161e8aa60b20d6864cc3ebb7eeec8
方洋 <18069898338@163.com>
2019-10-26 16:31:34 +0800

    项目初始化init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// 1.0 测试的数据
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
