d5c75c0adac31f06c0207c0de9fc07c483bd6547
方洋 <18069898338@163.com>
2019-10-26 16:44:27 +0800

    项目初始化init222

--- src/main/java/com/example/demo/web/HomeResource.java
+++ src/main/java/com/example/demo/web/HomeResource.java
@@ -1,5 +1,8 @@
 package com.example.demo.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * <提示>
  *
@@ -7,5 +10,17 @@ package com.example.demo.web;
  * @create 2019-10-26
  * @since 1.0.0
  */
+
+@RestController
 public class HomeResource {
+
+    /**
+     * 测试的第一行的代码
+     *
+     * @return the String
+     */
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello World";
+    }
 }
